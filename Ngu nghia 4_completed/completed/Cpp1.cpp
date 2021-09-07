ed082f1f732a0d2396469c684d0b675a4e053a5f
vananhbui41 <vananhbui41@gmail.com>
2021-09-07 21:27:57 +0700

    semantic4
    Ngữ nghĩa 4 complete

---
+++ Ngu nghia 4_completed/completed/Cpp1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int *ptr=5;
+	printf("%x",ptr);
+getch();
+return 1
+}
---
+++ Ngu nghia 4_completed/completed/Untitled1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    structure hotel
+    {
+        int items;
+        char name[10];
+    }a;
+    strcpy(a.name, "TAJ");
+    a.items=10;
+    printf("%s", a.name);
+    return 0;
+}
